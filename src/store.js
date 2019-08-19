ed3a152027471cbca430476e3bcbeba657e88f10
James Cat <james@catplusplus.org.uk>
2019-08-19 12:56:58 +0100

    add logging to redux middleware

--- src/store.js
+++ src/store.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import promise from 'redux-promise-middleware' 
-
+import promise from 'redux-promise-middleware'; 
+import { createLogger } from 'redux-logger';
 import rootReducer from './rootReducer';
 
 // eslint-disable-next-line no-underscore-dangle
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const middlewares = [promise];
+const logger = createLogger({});
+const middlewares = [logger, promise];
 
 const store = createStore(
   rootReducer,
